ed0c4d949cfc4f1f64af1654788481a5063798cc
Chijioke Chika <8pluscreative@gmail.com>
2023-06-04 05:47:51 +0100

    Entry Page

--- lib/pages/application/application_page.dart
+++ lib/pages/application/application_page.dart
@@ -1,5 +1,7 @@
+import 'package:elearn_app/common/values/colors.dart';
 import 'package:elearn_app/pages/application/widgets/application_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ApplicationPage extends StatefulWidget {
   const ApplicationPage({super.key});
@@ -9,13 +11,122 @@ class ApplicationPage extends StatefulWidget {
 }
 
 class _ApplicationPage extends State<ApplicationPage> {
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
       child: SafeArea(
           child: Scaffold(
-        body: buildPage(0),
+        body: buildPage(_index),
+        bottomNavigationBar: Container(
+          width: 375.w,
+          height: 58.h,
+          decoration:
+              BoxDecoration(color: AppColors.primaryElement, boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 1,
+              blurRadius: 1
+            ),
+          ]),
+          child: BottomNavigationBar(
+              currentIndex: _index,
+              elevation: 0,
+              type: BottomNavigationBarType.fixed,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              selectedItemColor: AppColors.primaryElement,
+              unselectedItemColor: AppColors.primaryFourElementText,
+              onTap: (value) {
+                setState(() {
+                  _index = value;
+                });
+              },
+              items: [
+                BottomNavigationBarItem(
+                  label: "Home",
+                  icon: SizedBox(
+                    width: 20.w,
+                    height: 20.h,
+                    child: Image.asset("assets/icons/home.png"),
+                  ),
+                  activeIcon: SizedBox(
+                    width: 20.w,
+                    height: 20.h,
+                    child: Image.asset(
+                      "assets/icons/home.png",
+                      color: AppColors.primaryElement,
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: "Search",
+                  icon: SizedBox(
+                    width: 20.w,
+                    height: 20.h,
+                    child: Image.asset("assets/icons/search2.png"),
+                  ),
+                  activeIcon: SizedBox(
+                    width: 20.w,
+                    height: 20.h,
+                    child: Image.asset(
+                      "assets/icons/search2.png",
+                      color: AppColors.primaryElement,
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: "Course",
+                  icon: SizedBox(
+                    width: 20.w,
+                    height: 20.h,
+                    child: Image.asset("assets/icons/play-circle1.png"),
+                  ),
+                  activeIcon: SizedBox(
+                    width: 20.w,
+                    height: 20.h,
+                    child: Image.asset(
+                      "assets/icons/play-circle1.png",
+                      color: AppColors.primaryElement,
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: "Chat",
+                  icon: SizedBox(
+                    width: 20.w,
+                    height: 20.h,
+                    child: Image.asset("assets/icons/message-circle.png"),
+                  ),
+                  activeIcon: SizedBox(
+                    width: 20.w,
+                    height: 20.h,
+                    child: Image.asset(
+                      "assets/icons/message-circle.png",
+                      color: AppColors.primaryElement,
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: "Profile",
+                  icon: SizedBox(
+                    width: 20.w,
+                    height: 20.h,
+                    child: Image.asset("assets/icons/person2.png"),
+                  ),
+                  activeIcon: SizedBox(
+                    width: 20.w,
+                    height: 20.h,
+                    child: Image.asset(
+                      "assets/icons/person2.png",
+                      color: AppColors.primaryElement,
+                    ),
+                  ),
+                ),
+              ]),
+        ),
       )),
     );
   }

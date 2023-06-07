a59247a442293b84e205ae9e1be61a34287242fa
Chijioke Chika <8pluscreative@gmail.com>
2023-06-07 13:32:38 +0100

    Home Page

--- lib/pages/home/widgets/home_page_widgets.dart
+++ lib/pages/home/widgets/home_page_widgets.dart
@@ -1,3 +1,4 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:elearn_app/common/values/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -121,22 +122,40 @@ Widget searchBox() {
 
 // SLIDER VIEW
 Widget sliderView() {
-  return Container(
-    margin: EdgeInsets.only(top: 20.h),
-    width: 325.w,
-    height: 160.h,
-    child: PageView(
-      children: [
-        _sliderContainer(imagePath: "assets/icons/art.png"),
-        _sliderContainer(imagePath: "assets/icons/image_1.png"),
-        _sliderContainer(imagePath: "assets/icons/image_2.png"),
-      ],
-    ),
+  return Column(
+    children: [
+      Container(
+        margin: EdgeInsets.only(top: 20.h),
+        width: 325.w,
+        height: 160.h,
+        child: PageView(
+          children: [
+            _sliderContainer(imagePath: "assets/icons/art.png"),
+            _sliderContainer(imagePath: "assets/icons/image_1.png"),
+            _sliderContainer(imagePath: "assets/icons/image_2.png"),
+          ],
+        ),
+      ),
+      Container(
+        child: DotsIndicator(
+          dotsCount: 3,
+          position: 1,
+          decorator: DotsDecorator(
+              color: AppColors.primaryThreeElementText,
+              activeColor: AppColors.primaryElement,
+              size: const Size.square(5.0),
+              activeSize: const Size(17.0, 5.0),
+              activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              )),
+        ),
+      )
+    ],
   );
 }
 
 //SLIDER CONTAINER
-Widget _sliderContainer({String imagePath="assets/icons/art.png"}){
+Widget _sliderContainer({String imagePath = "assets/icons/art.png"}) {
   return Container(
     width: 325.w,
     height: 160.h,

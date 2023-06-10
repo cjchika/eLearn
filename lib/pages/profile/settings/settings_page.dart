9cbd9883addd53d1f451308dfd829e682786045a
Chijioke Chika <8pluscreative@gmail.com>
2023-06-10 06:06:45 +0100

    Profile/Settings Screen

--- lib/pages/profile/settings/settings_page.dart
+++ lib/pages/profile/settings/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:elearn_app/pages/profile/settings/bloc/settings_blocs.dart';
+import 'package:elearn_app/pages/profile/settings/widgets/settings_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -15,12 +16,13 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildAppBar(),
       body: SingleChildScrollView(child:
           BlocBuilder<SettingsBlocs, SettingStates>(builder: (context, state) {
         return Container(
           child: Column(
             children: [
-              Text("Hello there"),
+              Center(child: Text("Hello there")),
             ],
           ),
         );

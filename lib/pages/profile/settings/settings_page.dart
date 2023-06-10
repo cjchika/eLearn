47fa7bb57fcf2a474cb273aea0568bb075c8b849
Chijioke Chika <8pluscreative@gmail.com>
2023-06-10 06:16:15 +0100

    Profile/Settings Screen

--- lib/pages/profile/settings/settings_page.dart
+++ lib/pages/profile/settings/settings_page.dart
@@ -2,6 +2,7 @@ import 'package:elearn_app/pages/profile/settings/bloc/settings_blocs.dart';
 import 'package:elearn_app/pages/profile/settings/widgets/settings_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'bloc/settings_states.dart';
 
@@ -16,13 +17,34 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: buildAppBar(),
       body: SingleChildScrollView(child:
           BlocBuilder<SettingsBlocs, SettingStates>(builder: (context, state) {
         return Container(
           child: Column(
             children: [
-              Center(child: Text("Hello there")),
+              GestureDetector(
+                onTap: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Confirm logout"),
+                          content: Text("Are you sure you want to logout?"),
+                        );
+                      });
+                },
+                child: Container(
+                  height: 100.w,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.fitHeight,
+                      image: AssetImage("assets/icons/Logout.png"),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         );

9e56c240d09279edde676d2611a805c4226c717a
Chijioke Chika <8pluscreative@gmail.com>
2023-06-11 23:36:37 +0100

    Profile/Settings Screen

--- lib/pages/profile/settings/settings_page.dart
+++ lib/pages/profile/settings/settings_page.dart
@@ -7,7 +7,6 @@ import 'package:elearn_app/pages/profile/settings/bloc/settings_blocs.dart';
 import 'package:elearn_app/pages/profile/settings/widgets/settings_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'bloc/settings_states.dart';
 
